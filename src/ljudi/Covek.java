a7aa25a4e86cac9c918f8b508fe1aa7e28f3a479
luka996 <luka_ilic@outlook.com>
2018-11-17 15:13:54 +0100

    Inicijalni komit

---
+++ src/ljudi/Covek.java
@@ -0,0 +1,13 @@
+package ljudi;
+
+public class Covek {
+	private String ime;
+	private String prezime;
+	int kilaza;
+	
+	public Covek(String ime, String prezime, int kilaza) {
+		this.ime = ime;
+		this.prezime = prezime;
+		this.kilaza = kilaza;
+	}
+}
